2da08ae9b0f03ed4f8d227b63d28811787c9115e
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-10-23 17:07:17 +0200

    Add login and registration page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,4 +69,10 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # For devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # For letter_opener
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
