577dec59093b9132458ea9510ed38765b3bba45e
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-10-23 23:35:02 +0200

    Add and generate user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :recipes, foreign_key: :user_id, dependent: :destroy
+  has_many :foods, foreign_key: :user_id, dependent: :destroy
+  validates :name, presence: true
 end
