59c151324a5da2739b1a5cdaea7f33d3731004c2
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-10-27 22:13:03 +0430

    Update app/models/food.rb file

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,6 +4,6 @@ class Food < ApplicationRecord
 
   validates :name, presence: true
   validates :measurement_unit, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: 'only allows letters' }
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0, allow_float: true }
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
